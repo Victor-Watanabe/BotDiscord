08877d0f65b363ddaf42f2795e3f8670eb24f55a
Victor-Watanabe <watavh@hotmail.com>
2024-04-22 22:29:32 -0300

    embed button aesthetic

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 
+
 intents = discord.Intents.default()
 
 # Capacitando o Bot Para Leitura de Mensagens
@@ -20,7 +21,7 @@ async def ola(ctx: commands.Context):
 
 # Criando Embed para Boas Vindas do Usuário
 @bot.event
-async def on_member_join(member:discord.Member):
+async def on_member_join(member: discord.Member):
     # Identificando o Canal de Recepção
     channel = bot.get_channel(1230590230323793995)
 
@@ -32,20 +33,35 @@ async def on_member_join(member:discord.Member):
 
     # Tratamento de Imagens e Visual de Embed
     image_author = discord.File('images/lucas_developer.png', 'lucas_developer.png')
-    my_embed.set_author(name="Lucas Batista", url='https://www.instagram.com/lucasbatista.apk/', icon_url='attachment://lucas_developer.png')
-    image_thumbnail = discord.File('images/thumbnail.jpeg', 'thumbnail.jpeg')
-    my_embed.set_thumbnail(url = "attachment://thumbnail.jpeg")
-    my_embed.color = discord.Color.dark_blue()
+    my_embed.set_author(name="Lucas Batista", url='https://febatisplay.com/', icon_url='attachment://lucas_developer.png')
+    image_thumbnail = discord.File('images/simple_thumbnail.jpeg', 'simple_thumbnail.jpeg')
+    my_embed.set_thumbnail(url="attachment://simple_thumbnail.jpeg")
+    my_embed.color = discord.Color.dark_green()
     my_embed.set_footer(text="Foque nos Estudos, Boa Sorte!")
 
-    await channel.send(files = [image_thumbnail, image_author], embed=my_embed)
-    try:
-        await member.create_dm()
-        await member.dm_channel.send("Olá, sou o Bot do Servidor do Watanabe_Developer. Por favor, digite seu e-mail:")
-        print(f'Mensagem enviada para {member.name}')
-    except discord.Forbidden:
-        print(f'Não foi possível enviar mensagem para {member.name}. Permissão de enviar mensagens diretas pode estar faltando.')
+    # Função para criar o botão
+    async def button(ctx: commands.Context):
+        view = discord.ui.View()
+        acess_button = discord.ui.Button(label="Acessar!", style=discord.ButtonStyle.green)
+        support_button = discord.ui.Button(label="Suporte.", url="https://discord.com/channels/1230590230323793992/1232132723888619560", style=discord.ButtonStyle.link)
 
+    # Adicionando o botão de acesso
+        view.add_item(acess_button)  
+    
+    # Adicionando o botão de suporte
+        view.add_item(support_button)
+        return view
+
+    # Adicionando a visualização com o botão à mensagem de boas-vindas
+    view = await button(channel)  # Chamando a função button para obter a visualização
+    await channel.send(files= [image_author, image_thumbnail], embed=my_embed, view=view)  # Enviando a mensagem com o embed e a visualização
+
+# Lógica dos botões!
+@bot.event
+async def on_button_click(interect: discord.Interaction):
+    if interect.component.custom_id == "acessar_button":
+        await interect.response.send_message("Por favor, digite seu e-mail:")
+      
 
 # Verificando o Email Informado
 @bot.command()
@@ -61,5 +77,6 @@ async def on_ready():
 # Identificando o Bot com o Token de Acesso e Colocando o bot para funcionar!
 bot.run("MTIzMDYwMDE2NDg0MzE5MjM1NQ.Gix1Vw.gFCOcnfI2pVzLNOOE2YaKssvCfUo-0KPG4xJXs")
 
-# verificar ephemeral
-# Configurar botões de acesso ao formulário e guardar o email em uma variável. 
+# Verificar lógica de programação para apresentar um comando de input após pressionado botão.
+# Verificar como funciona o armazenamento da variável 
+# Estudar como utilizar a API do google Sheets
