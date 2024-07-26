8a257f72262e915cfd421d0b0ff6ddeddf20b4c7
Victor-Watanabe <watavh@hotmail.com>
2024-07-26 19:53:00 -0300

    fine adjustments

--- bot.py
+++ bot.py
@@ -1,103 +1,111 @@
 import discord
 from discord.ext import commands
-from discord.ui import Button
+from discord.ui import Button, View, Modal, TextInput
 from google_sheet import main, secondary
 
-
 # Lógica do Formulário de Email
-class email_form(discord.ui.Modal):
+class EmailForm(Modal):
     def __init__(self):
         super().__init__(title="Email de Aquisição do Curso.")
+        self.email_input = TextInput(label="Email", placeholder="Exemplo: Exemplo@gmail.com", custom_id="email_field")
+        self.add_item(self.email_input)  # Certifique-se de adicionar o campo ao modal
 
-    email_input = discord.ui.TextInput(label="Email", placeholder="Exemplo: Exemplo@gmail.com", custom_id="email_field")
-    async def on_submit(self, interect:discord.Interaction):
-        global user
-        user = interect.user
-        global email
+    async def on_submit(self, interaction: discord.Interaction):
+        user = interaction.user
         email = self.email_input.value
         username = user.name
         reply = main(email, username)
-        if reply == False:
-            await interect.response.send_message(f"Senhor {user}, o email {email} NÃO foi localizado no banco de dados, Favor entre em contato com o nosso canal de Suporte!", ephemeral = True)
+        if not reply:
+            await interaction.response.send_message(
+                f"Senhor {user}, o email {email} NÃO foi localizado no banco de dados, Favor entre em contato com o nosso canal de Suporte!",
+                ephemeral=True)
         else:
-            role = user.guild.get_role(1230596024041607220)
-            
+            role = user.guild.get_role(123456789012345678)  # Insira o ID correto do role aqui
             await user.add_roles(role)
-            await interect.response.send_message(f"Senhor {user}, seu Cadastro foi Concluído, Bem Vindo a Nossa Comunidade!", ephemeral = True)
+            await interaction.response.send_message(
+                f"Senhor {user}, seu Cadastro foi Concluído, Bem Vindo a Nossa Comunidade!", ephemeral=True)
 
 intents = discord.Intents.default()
-
-# Capacitando o Bot Para Leitura de Mensagens
 intents.message_content = True
-
-# Capacitando o Bot Para Identificação de Membros do Servidor!
 intents.members = True
 
-# Adicionando um Prefixo, ou seja, uma String Para Identificação de Comando, Nesse Caso, "/"
 bot = commands.Bot(command_prefix="/", intents=intents)
 
-# Criando Comando Responsivo à Mensagem "ola"
 @bot.command()
 async def ola(ctx: commands.Context):
     user = ctx.author
     await ctx.reply(f"Hello {user.display_name}!")
 
-# Criando Embed para Boas Vindas do Usuário
 @bot.event
 async def on_member_join(member: discord.Member):
-    # Identificando o Canal de Recepção
-    channel = bot.get_channel(1230590230323793995)
+    channel = bot.get_channel()
 
-    # Título e Descrição
-    my_embed = discord.Embed(title=f"Bem-vindo(a) ao Servidor Desenvolvedor(a), {member.name}!")
-    my_embed.description = """Para garantir acesso ao servidor, basta clicar no botão abaixo e preencher o formulário com seu e-mail vinculado à compra. Seu acesso será liberado automaticamente.
+    my_embed = discord.Embed(
+        title=f"Bem-vindo(a) ao Servidor Desenvolvedor(a){member.name}!",
+        description="""Para garantir acesso ao servidor, basta clicar no botão abaixo e preencher o formulário com seu e-mail vinculado à compra. Seu acesso será liberado automaticamente.
 
-    Caso ocorra algum erro, entre em contato com nosso suporte!"""
+Caso ocorra algum erro, entre em contato com nosso suporte!""",
+        color=discord.Color.dark_green()
+    )
 
-    # Tratamento de Imagens e Visual de Embed
+    my_embed.set_footer(text="Foco nos Estudos, Boa Sorte!")
+
+    view = create_button_view()
+    await send_welcome_message(channel, my_embed, view)
+
+def create_button_view():
+    async def response_button(interact: discord.Interaction):
+        await interact.response.send_modal(EmailForm())
+        
+    async def button_reloading(interact: discord.Interaction):
+        my_embed = discord.Embed(
+        title=f"Bem-vindo(a) ao Servidor Desenvolvedor(a)",
+        description="""Para garantir acesso ao servidor, basta clicar no botão abaixo e preencher o formulário com seu e-mail vinculado à compra. Seu acesso será liberado automaticamente.
+
+Caso ocorra algum erro, entre em contato com nosso suporte!""",
+        color=discord.Color.dark_green()
+    )
+
+        my_embed.set_footer(text="Foco nos Estudos, Boa Sorte!")
+        image_author = discord.File('images/lucas_developer.png', 'lucas_developer.png')
+        my_embed.set_author(name="Lucas Batista", url='https://febatisplay.com/', icon_url='attachment://lucas_developer.png')
+        image_thumbnail = discord.File('images/simple_thumbnail.jpeg', 'simple_thumbnail.jpeg')
+        my_embed.set_thumbnail(url="attachment://simple_thumbnail.jpeg")
+
+        view = create_button_view()
+        await interact.response.send_message(files=[image_author, image_thumbnail], embed=my_embed, view=view, ephemeral=True)
+        
+    view = View()
+    reloading_button = Button(label="Recarregar", style=discord.ButtonStyle.blurple, emoji="🔁", custom_id="reloading")
+    support_button = Button(label="Suporte", url="https://discord.com/channels/1230590230323793992/1232132723888619560", style=discord.ButtonStyle.link, emoji="💬")
+    access_button = Button(label="Acessar!", style=discord.ButtonStyle.green, emoji="✔️", custom_id="access_button")
+
+    access_button.callback = response_button
+    reloading_button.callback = button_reloading
+
+    view.add_item(access_button)
+    view.add_item(reloading_button)
+    view.add_item(support_button)
+
+    return view
+
+async def send_welcome_message(channel, my_embed, view):
     image_author = discord.File('images/lucas_developer.png', 'lucas_developer.png')
     my_embed.set_author(name="Lucas Batista", url='https://febatisplay.com/', icon_url='attachment://lucas_developer.png')
     image_thumbnail = discord.File('images/simple_thumbnail.jpeg', 'simple_thumbnail.jpeg')
     my_embed.set_thumbnail(url="attachment://simple_thumbnail.jpeg")
-    my_embed.color = discord.Color.dark_green()
-    my_embed.set_footer(text="Foco nos Estudos, Boa Sorte!")
+    await channel.send(files=[image_author, image_thumbnail], embed=my_embed, view=view)
 
-    # Função para criar o botão
-    async def button(ctx: commands.Context):
-        async def response_button(interact:discord.Interaction):
-            await interact.response.send_modal(email_form())
-        view = discord.ui.View()
-        acess_button = Button(label="Acessar!", style=discord.ButtonStyle.green, emoji="✔️", custom_id="acessar_button")
-        acess_button.callback = response_button
-        support_button = Button(label="Suporte.", url="https://discord.com/channels/1230590230323793992/1232132723888619560", style=discord.ButtonStyle.link, emoji="💬")
-
-        # Adicionando o botão de acesso
-        view.add_item(acess_button)  
-    
-        # Adicionando o botão de suporte
-        view.add_item(support_button)
-        return view
-
-    # Adicionando a visualização com o botão à mensagem de boas-vindas
-    view = await button(channel)  # Chamando a função button para obter a visualização
-    await channel.send(files= [image_author, image_thumbnail], embed=my_embed, view=view)  # Enviando a mensagem com o embed e a visualização
-
-# alterando status do usuário no banco de dados
 @bot.event
 async def on_member_remove(member: discord.Member):
     name = member.name
-    response = secondary(name)
-    pass
-    # FUNÇÃO PARA TORNAR AUSENTE!
-    
+    secondary(name)
 
-# Enviando uma Mensagem Sempre que o Bot for Inicializado!
 @bot.event
 async def on_ready():
     print("I'm Ready!")
-
 # Identificando o Bot com o Token de Acesso e Colocando o bot para funcionar!
-bot.run("MTIzMDYwMDE2NDg0MzE5MjM1NQ.Gix1Vw.gFCOcnfI2pVzLNOOE2YaKssvCfUo-0KPG4xJXs")
+bot.run("")
 
 
 # LEMBRAR DE ALTERAR TODOS OS ID DE COMANDO 
--- google_sheet.py
+++ google_sheet.py
@@ -1,14 +1,12 @@
-import os.path
+import os
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from google.auth.exceptions import RefreshError  # Importando a exceção RefreshError
 
-# If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
-
-# The ID and range of a sample spreadsheet.
 SAMPLE_SPREADSHEET_ID = "18kaJLF0OBLkMNS9fKx4Q_HPYQZ37So91-NXr7WURL2c"
 SAMPLE_RANGE_NAME = "WorkSheet!C2:C"
 
@@ -17,127 +15,77 @@ new_status = [["Na Comunidade"]]
 old_status = ['Na Comunidade']
 
 def string_sublist(sublist):
-      return ' '.join(map(str, sublist))
+    return ' '.join(map(str, sublist))
+
+def get_credentials():
+    creds = None
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            try:
+                creds.refresh(Request())
+            except RefreshError as e:
+                print("Erro ao atualizar token: ", e)
+                os.remove("token.json")
+                return get_credentials()
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+    return creds
 
 def main(email, username):
-
-  creds = None
-  
-  if os.path.exists("token.json"):
-    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(
-          "credentials.json", SCOPES
-      )
-      creds = flow.run_local_server(port=0)
-    
-    with open("token.json", "w") as token:
-      token.write(creds.to_json())
-
-  try:
-    service = build("sheets", "v4", credentials=creds)
-
-    sheet = service.spreadsheets()
-    result = (
-        sheet.values()
-        .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME)
-        .execute()
-    )
-    values = result.get("values", [])
-    user_data = [[username]]
-    
-    # Mapear cada sublista para uma string usando map
-    string_list = list(map(string_sublist, values))
-     
-    if values:
-        if email in string_list:
-          position = string_list.index(email) + 2
-          SAMPLE_RANGE_NAME_WRITE_STATUS = "WorkSheet!G{}".format(position)
-          result_status = (
-              sheet.values()
-              .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME_WRITE_STATUS)
-              .execute()
-          )
-          values_status = result_status.get("values", [])
-          status_list = list(map(string_sublist, values_status)) 
-          if old_status == status_list:
-            return False
-          else:
-            status = (
-              sheet.values()
-              .update(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME_WRITE_STATUS, valueInputOption = "USER_ENTERED", body={"values": new_status}).execute()
-          )
-            SAMPLE_RANGE_NAME_WRITE_USERNAME = "WorkSheet!H{}".format(position)
-            user_api = (
-              sheet.values()
-              .update(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME_WRITE_USERNAME, valueInputOption = "USER_ENTERED", body={"values": user_data}).execute()
-          )
-            return True
+    creds = get_credentials()
+    try:
+        service = build("sheets", "v4", credentials=creds)
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME).execute()
+        values = result.get("values", [])
+        user_data = [[username]]
+        string_list = list(map(string_sublist, values))
+        
+        if values:
+            if email in string_list:
+                position = string_list.index(email) + 2
+                SAMPLE_RANGE_NAME_WRITE_STATUS = f"WorkSheet!G{position}"
+                result_status = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME_WRITE_STATUS).execute()
+                values_status = result_status.get("values", [])
+                status_list = list(map(string_sublist, values_status))
+                if old_status == status_list:
+                    return False
+                else:
+                    sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME_WRITE_STATUS, valueInputOption="USER_ENTERED", body={"values": new_status}).execute()
+                    SAMPLE_RANGE_NAME_WRITE_USERNAME = f"WorkSheet!H{position}"
+                    sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME_WRITE_USERNAME, valueInputOption="USER_ENTERED", body={"values": user_data}).execute()
+                    return True
+            else:
+                return False
         else:
-            return False
-    else:
-        print("Nenhum dado retornado.")
-  except HttpError as err:
-    print(err)
+            print("Nenhum dado retornado.")
+    except HttpError as err:
+        print("Erro de HTTP: ", err)
 
-# The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = "18kaJLF0OBLkMNS9fKx4Q_HPYQZ37So91-NXr7WURL2c"
-SAMPLE_RANGE_USERNAME = "WorkSheet!H2:H"
-
-def secondary (name):
-
-  creds = None
-  
-  if os.path.exists("token.json"):
-    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(
-          "credentials.json", SCOPES
-      )
-      creds = flow.run_local_server(port=0)
-    
-    with open("token.json", "w") as token:
-      token.write(creds.to_json())
-
-  try:
-    service = build("sheets", "v4", credentials=creds)
-
-    sheet = service.spreadsheets()
-    result = (
-        sheet.values()
-        .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_USERNAME)
-        .execute()
-    )
-    values = result.get("values")
-      
-    # Mapear cada sublista para uma string usando map
-    string_list = list(map(string_sublist, values))
-   
-    if values:
-        if name in string_list:
-            position = string_list.index(name) + 2
-            SAMPLE_RANGE_NAME_WRITE_STATUS = "WorkSheet!G{}".format(position)
-            status = (
-              sheet.values()
-              .update(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME_WRITE_STATUS, valueInputOption = "USER_ENTERED", body={"values": other_status}).execute()
-          )
+def secondary(name):
+    creds = get_credentials()
+    try:
+        service = build("sheets", "v4", credentials=creds)
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range="WorkSheet!H2:H").execute()
+        values = result.get("values", [])
+        string_list = list(map(string_sublist, values))
+        
+        if values:
+            if name in string_list:
+                position = string_list.index(name) + 2
+                SAMPLE_RANGE_NAME_WRITE_STATUS = f"WorkSheet!G{position}"
+                sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME_WRITE_STATUS, valueInputOption="USER_ENTERED", body={"values": other_status}).execute()
         else:
-            pass
-    else:
-        print("Nenhum dado retornado.")
-  except HttpError as err:
-    print(err)
-
-
-
+            print("Nenhum dado retornado.")
+    except HttpError as err:
+        print("Erro de HTTP: ", err)
 
 if __name__ == "__main__":
-  main()
+    main("example@example.com", "username")
